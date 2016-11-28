479daff34b107d96744d89c7eb396465c582fbc1
Connor Whisman <ConnorWhisman@Connors-MacBook-Pro.local>
2016-11-27 19:32:50 -0500

    Working except for same numbers every time
    error handling when number is null, loops over list of dicts to display
    random numbers

--- main.py
+++ main.py
@@ -8,13 +8,23 @@ app.debug = True
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'GET':
-        return render_template('index.html')
-    elif request.method == 'POST':
-        tickets = request.form['numofticks']
-        return render_template('index.html', tickets = tickets)
-    else:
-        return "Invlalid Request"
+    try:
+        if request.method == 'GET':
+            return render_template('index.html')
+        elif request.method == 'POST':
+            requestdata = request.form['numofticks']
+            tickets = int(requestdata)
+            numbers = {}
+            boards = []
+            while len(boards) < tickets:
+                numbers = {randint(1, 50): 'one', randint(1, 50): 'two', randint(1, 50): 'three', randint(1, 50): 'four', randint(1, 50): 'five'}
+                boards.append(dict(numbers))
+            return render_template('index.html', tickets = tickets, boards = boards, numbers = numbers)
+        else:
+            return 'Invlalid Request'
+    except:
+        numerror = 'Please enter a number'
+        return render_template('index.html', numerror = numerror)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
