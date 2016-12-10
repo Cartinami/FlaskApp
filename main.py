8a5d70a360b51a2d656c0fd211a19b34e12e8833
Connor Whisman <ConnorWhisman@Connors-MacBook-Pro.local>
2016-12-10 00:06:35 -0500

    Coming along..
    simple css, done for now. money and prices are starting to take shape.
    winning stats improved. database work is next

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from flask import Flask, jsonify, render_template, request, redirect, url_for
-from flask_bootstrap import Bootstrap
 from random import randint, seed
 
 app = Flask(__name__)
@@ -12,46 +11,79 @@ def index():
 
 @app.route('/powerball', methods=['GET', 'POST'])
 def powerball():
-    money = 0
-    if request.method == 'GET':
-        return render_template('powerball.html', money=money)
+    money = 20
+    ticketprice = -4
+    totalprice = 0
+    winningnumbers = ['','','','','']
 
-    if request.method == 'POST':
+    if request.method == 'GET':
+        return render_template('powerball.html', money=money, winningnumbers=winningnumbers)
+    elif request.method == 'POST':
         requestdata = request.form['numofticks']
         tickets = int(requestdata)
         winningnumbers = [randint(1,10), randint(1,10), randint(1,10), randint(1,10), randint(1,10)]
         boards = []
         stats = []
-        while len(boards) < tickets:
-            one = randint(1,10)
-            two = randint(1,10)
-            three = randint(1,10)
-            four = randint(1,10)
-            five = randint(1,10)
-            boards.append(list([one,two,three,four,five]))
-            if ((one == winningnumbers[0]) and (two == winningnumbers[1]) and (three == winningnumbers[2]) and (four == winningnumbers[3]) and (five == winningnumbers[4])):
-                status = 'WINNER'
-                money = money + 1200
-            elif ((one == winningnumbers[0]) and (two == winningnumbers[1]) and (three == winningnumbers[2]) and (four == winningnumbers[3])):
-                status = 'Four'
-                money = money + 1000
-            elif ((one == winningnumbers[0]) and (two == winningnumbers[1]) and (three == winningnumbers[2])):
-                status = 'Three'
-                money = money + 600
-            elif ((one == winningnumbers[0]) and (two == winningnumbers[1])):
-                status = 'Two'
-                money = money + 200
-            elif one == winningnumbers[0]:
-                status = 'One'
-                money = money + 40
-            else:
-                status = ''
-                money = money
-            stats.append(status)
-        return render_template('powerball.html',boards=boards,winningnumbers=winningnumbers,stats=stats,money=money)
+        indexes = []
+        matches = []
+        prizes = []
+        winstats = []
 
+        i = 0
+        while len(boards) < tickets:
+            if not (money < ticketprice):
+                money = money - ticketprice
+                totalprice = totalprice + ticketprice
+                one = randint(1,10)
+                two = randint(1,10)
+                three = randint(1,10)
+                four = randint(1,10)
+                five = randint(1,10)
+                boards.append(list([one,two,three,four,five]))
+
+                if ((one == winningnumbers[0]) and (two == winningnumbers[1]) and (three == winningnumbers[2]) and (four == winningnumbers[3]) and (five == winningnumbers[4])):
+                    status = '!!!!!!!WINNER!!!!!!!'
+                    amount = 1200
+                    money = money + amount
+                elif ((one == winningnumbers[0]) and (two == winningnumbers[1]) and (three == winningnumbers[2]) and (four == winningnumbers[3])):
+                    status = 'Four'
+                    amount = 1000
+                    money = money + amount
+                elif ((one == winningnumbers[0]) and (two == winningnumbers[1]) and (three == winningnumbers[2])):
+                    status = 'Three'
+                    amount = 600
+                    money = money + amount
+                elif ((one == winningnumbers[0]) and (two == winningnumbers[1])):
+                    status = 'Two'
+                    amount = 200
+                    money = money + amount
+                elif one == winningnumbers[0]:
+                    status = 'One'
+                    amount = 40
+                    money = money + amount
+                else:
+                    status = 'L'
+                    money = money
+                    amount = 0
+                net = money + totalprice
+                stats.append(status)
+
+                if len(stats[i]) > 1:
+                    index = str(i + 1)
+                    match = stats[i]
+                    prize = '+$' + str(amount) + '.00'
+                    winstats.append(list([index,match,prize]))
+                    i += 1
+                else:
+                    i += 1
+            else:
+                break
+        return render_template('powerball.html',boards=boards,winningnumbers=winningnumbers,money=money,winstats=winstats,totalprice=totalprice,net=net)
     else:
         return 'Invalid Request Method'
+
+
+
 
 if __name__ == '__main__':
     app.run()
